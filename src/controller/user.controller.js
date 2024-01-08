f27e8ad40c110f230c0505ce76536c96805a5623
Michail-Seglin <michailsavuskin13@gmail.com>
2024-01-08 09:49:34 +0300

    routePost

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,7 +1,15 @@
 const express = require('express');
-
+const { createUser } = require('../service/user.service');
 const route = express.Router();
 
+route.post('/', async (req, res) => {
+    try {
+        const data = await createUser(req.body);
+        res.send;
+    } catch (er) {
+        res.send(er.message);
+    }
+})
 
 
 module.exports = route;
---
+++ src/repository/user.repository.js
@@ -0,0 +1,9 @@
+const { TableUser, ObjectId } = require('../db');
+
+async function createUserDB(user) {
+    await TableUser.create(user);
+    const data = await TableUser.find();
+    return data;
+}
+
+module.exports = { createUserDB };
